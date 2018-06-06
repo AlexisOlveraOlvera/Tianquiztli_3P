0244bc5b94600267d125a930b9b11480fabe32fe
AlexisOlveraOlvera <36865105+AlexisOlveraOlvera@users.noreply.github.com>
2018-06-06 17:16:46 -0500

    Initial commit

---
+++ app/src/main/java/mecalogik/com/tianquiztli_3p/MainActivity.java
@@ -0,0 +1,13 @@
+package mecalogik.com.tianquiztli_3p;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
